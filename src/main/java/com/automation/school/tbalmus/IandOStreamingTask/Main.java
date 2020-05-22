b770f818043ab29dd3abc06ed3d5e6b97866732e
Tatiana Balmus <en_tbalmus@yahoo.com>
2020-05-22 11:54:49 +0300

    Done tasks Input and Output Streaming Tatiana Balmus

---
+++ src/main/java/com/automation/school/tbalmus/IandOStreamingTask/Main.java
@@ -0,0 +1,39 @@
+package com.automation.school.tbalmus.IandOStreamingTask;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main ( String[] args ) throws IOException {
+// declare files
+        FileReader inFile;
+        FileWriter outFile;
+        int countWord = 0;
+        String myWord = "fulfils";
+
+        inFile = new FileReader ( "src\\main\\resources\\wordlist23.txt" );
+        outFile = new FileWriter ( "src\\main\\resources\\results.txt" );
+        BufferedReader reader = new BufferedReader ( inFile );
+
+        ArrayList < Long > lineNumberList = new ArrayList <> ( );
+        long lineNumber = 1;
+        String liner;
+        while ((liner = reader.readLine ( )) != null) {
+            lineNumber++;
+            if (liner.equals ( "fulfils" )) {
+                countWord++;
+                lineNumberList.add ( lineNumber );
+            }
+        }
+
+        outFile.write ( "The word " + myWord + " was found " + countWord + " times " + "on lines: " + lineNumberList );
+
+        System.out.println ( lineNumberList );
+
+
+        inFile.close ( );
+        outFile.close ( );
+        System.out.println ( "The word " + myWord + " was found " + countWord + " times." );
+    }
+}
