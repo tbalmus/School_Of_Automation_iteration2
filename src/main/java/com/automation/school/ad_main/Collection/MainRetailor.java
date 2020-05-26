4e9b7b08dff28489f0f4716f9e4a8fa2f465403e
adiogoti <andrei.diogoti@endava.com>
2020-05-26 18:50:47 +0300

    Extra comment for CollectionTask resolved

--- src/main/java/com/automation/school/ad_main/Collection/MainRetailor.java
+++ src/main/java/com/automation/school/ad_main/Collection/MainRetailor.java
@@ -8,49 +8,48 @@ public class MainRetailor {
         //Start
         System.out.println("Hello to  create new oder: 1) enter user name 2) add Address 3) enter item ID");
         // New user registration
-        Scanner myNewUserObj = new Scanner(System.in);  // Create a Scanner object
+        Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Hello please create new User");
-        String user = myNewUserObj.nextLine(); // Enter username and press Enter
+        String user = myScanner.nextLine(); // Enter username and press Enter
         System.out.println("Username is: " + user);
-        Scanner enterFullAddress1 = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter Country, City and street:");
-        String newAddressCountry1 = enterFullAddress1.nextLine();
-        String newAddressCity1 = enterFullAddress1.nextLine();
-        String newAddressStreet1 = enterFullAddress1.nextLine();
-        Address address1 = new Address(newAddressCountry1, newAddressCity1, newAddressStreet1);
+        String newAddressCountry1 = myScanner.nextLine();
+        String newAddressCity1 = myScanner.nextLine();
+        String newAddressStreet1 = myScanner.nextLine();
+        Address address1 = new Address();
+        address1.addAddress(newAddressCountry1, newAddressCity1, newAddressStreet1);
         User user1 = new User();
         user1.createUser(user);
         user1.addAddress("Home", address1);
 
-        System.out.println("User is created :"+ user1);
+        System.out.println("User is created :" + user1);
         // Create Adress
-
-
 
 
         String newAddressCountry2 = "Moldova";
         String newAddressCity2 = "Chishinau";
         String newAddressStreet2 = "Nunta 19";
-        Address address2 = new Address(newAddressCountry2, newAddressCity2, newAddressStreet2);
+        Address address2 = new Address();
+        address2.addAddress(newAddressCountry2, newAddressCity2, newAddressStreet2);
         String newAddressCountry3 = "Moldova";
         String newAddressCity3 = "Orhey";
         String newAddressStreet3 = "Doga 19";
-        Address address3 = new Address(newAddressCountry3, newAddressCity3, newAddressStreet3);
+        Address address3 = new Address();
+        address3.addAddress(newAddressCountry3, newAddressCity3, newAddressStreet3);
 
-        Map<String, Address> addresses = new HashMap<String, Address>();
+        Map<String, Address> addresses = new HashMap<>();
         //Adding elements to map
         addresses.put("Home", address1);
         addresses.put("Work", address2);
         addresses.put("Home", address3);
-        for (Map.Entry m : addresses.entrySet()) {
-        }
+
 
         // create list of items
         Item myToy = new Item(1, "lego", Type.TOY, 50.23);
         Item myToy2 = new Item(2, "legoGo", Type.TOY, 20.31);
         Item myToy3 = new Item(3, "legoUP", Type.TOY, 52.4);
 
-        List<Item> listToys = new LinkedList<Item>();
+        List<Item> listToys = new LinkedList<>();
         listToys.add(myToy);
         listToys.add(myToy2);
         listToys.add(myToy3);
@@ -63,11 +62,11 @@ public class MainRetailor {
         }
         System.out.println("Hello please create new Order. Select item from the list " + newLine);
         for (String x : arrToys)
-            System.out.print("item list:" + x + " " +newLine);
+            System.out.print("item list:" + x + " " + newLine);
 
         User user2 = new User();
         user2.createUser("Oleg");
-        user2.addAddress("Office",address3);
+        user2.addAddress("Office", address3);
         // create User  Order
         UserOrder userOrder1 = new UserOrder();
         userOrder1.registrationUser(user2);
@@ -75,17 +74,17 @@ public class MainRetailor {
         userOrder1.addItem(myToy);
         userOrder1.addItem(myToy2);
         userOrder1.addItem(myToy3);
-        System.out.print("Order:" + userOrder1.toString() + " " +newLine);
+        System.out.print("Order:" + userOrder1.toString() + " " + newLine);
 
         UserOrder userOrder2 = new UserOrder();
         userOrder2.registrationUser(user1);
         userOrder2.addItem(myToy);
-        System.out.print("Order:" + userOrder2.toString() + " " +newLine);
+        System.out.print("Order:" + userOrder2.toString() + " " + newLine);
 
         // create Company order
         User user5 = new User();
         user5.createUser("user5");
-        user5.addAddress("Home",address2);
+        user5.addAddress("Home", address2);
         String companyName1 = "company1";
         String companyName2 = "company2";
         Company company1 = new Company();
@@ -93,9 +92,9 @@ public class MainRetailor {
         company1.createCompany(companyName1);
         company2.createCompany(companyName2);
         // add Address to Company
-        company1.addAddress("Office",address1);
-        company2.addAddress("Store",address3);
-        System.out.println(company1.toString()+newLine);
+        company1.addAddress("Office", address1);
+        company2.addAddress("Store", address3);
+        System.out.println(company1.toString() + newLine);
         CompanyOrder companyOrder1 = new CompanyOrder();
         companyOrder1.CreateOrder(user5, company1);
         // Create company2
@@ -110,8 +109,6 @@ public class MainRetailor {
         companyOrder1.addItem(myToy3);
 
         System.out.println(companyOrder1.toString() + newLine);
-        System.out.println(companyOrder2.toString()+ newLine);
-
-
+        System.out.println(companyOrder2.toString() + newLine);
     }
 }
--- src/main/java/com/automation/school/ad_main/Collection/UserOrder.java
+++ src/main/java/com/automation/school/ad_main/Collection/UserOrder.java
@@ -1,52 +1,40 @@
 package com.automation.school.ad_main.Collection;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class UserOrder implements Order {
-
     private Long userOrderId; //(generate automatically in constructor )
-    public static long userCurrentOrderId =0;
+    public static long userCurrentOrderId = 0;
     public LocalDateTime date; //(set as current day directly in constructor)
     public User user;
     public List<Item> itemsUser;
-    private static int nextUniqueId = 0;
-
 
     public UserOrder() {
         userCurrentOrderId++;
-
         this.date = LocalDateTime.now();
         this.itemsUser = new LinkedList<>();
     }
 
 
     @Override
-    public double sum() {
-        return 0;
-    }
-
-    @Override
-    public int count() {
-        return 0;
-    }
-
-    @Override
-    public LocalDateTime date() {
-        return null;
+    public double sum(double price) {
+        double[] arr = {price};
+        double total = 0;
+        for (double v : arr) {
+            total = total + v;
+        }
+        return total;
     }
 
     public void registrationUser(User user) {
         this.userOrderId = new IdGenerator().set(UserOrder.userCurrentOrderId);
         this.user = user;
-
     }
 
     public void addItem(Item items) {
-       this.itemsUser.add(items);
+         itemsUser.add(items);
     }
 
     @Override
